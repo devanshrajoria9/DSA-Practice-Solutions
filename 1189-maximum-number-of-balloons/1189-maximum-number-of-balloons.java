7737b69917690a97f8bd0b109860514f94c31822
Devansh Rajoriya <121275153+devanshrajoria9@users.noreply.github.com>
2026-08-12 22:01:37 +0530

    Time: 12 ms (10.46%), Space: 44.2 MB (6.5%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -2,16 +2,16 @@ class Solution {
     public int maxNumberOfBalloons(String text) {
         int n = text.length();
         HashMap<Character, Integer> have = new HashMap<>();
-        for(int i=0; i<n; i++){
-            char h = text.charAt(i);
-            have.put(h, have.getOrDefault(h, 0) + 1);
+        for(int i = 0; i < n; i++){
+            char ch = text.charAt(i);
+            have.put(ch, have.getOrDefault(ch, 0) + 1);
         }
         HashMap<Character, Integer> need = new HashMap<>();
-        need.put('b',1);
-        need.put('a',1);
-        need.put('l',2);
-        need.put('o',2);
-        need.put('n',1);
+        need.put('b', 1);
+        need.put('a', 1);
+        need.put('l', 2);
+        need.put('o', 2);
+        need.put('n', 1);
 
         int res = Integer.MAX_VALUE;
 
